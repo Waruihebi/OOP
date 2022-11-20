3dbc687f98d9245a52493293c53e52bfabeb3cd6
Waruihebi <115277925+Waruihebi@users.noreply.github.com>
2022-11-20 14:09:35 +0200

    Delete test directory

--- test/Lab2/DoubleRaitingGame.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lab2;
-
-class DoubleRatingGame : DefaultGame
-{
-    public DoubleRatingGame(DefaultAccount winner, DefaultAccount looser, uint rating) : base(winner, looser,2*rating)
-    {
-    }
-}
